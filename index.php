db47bb7a29458ea2b63be9cc1d1ee0b14be65d20
freddy barco <freddybarco@gmail.com>
2015-03-26 09:43:55 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+      <head>
+            
+            <meta charset="ISO-8859-1" />
+            <meta name="description"  content="Formulario de datos" />
+            <meta name="keywords" content="HTML5" />
+            <title>Login</title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            
+            
+      </head>
+<body>
+
+
+<div id="main">
+<hr>
+<div id="login">
+<h2>Login</h2>
+<hr/>   
+
+
+<form action="login.php" method="post">
+<label>UserName  :</label>
+<input type="text" name="user" id="name" placeholder="username"/><br /><br />
+<label>Password  :</label>
+<input type="password" name="password" id="password" placeholder="**********"/><br/><br />
+<input type="submit" value=" Login " name="submit"/><br />
+<span></span>
+</form>
+</div>
+</div>
+      
+
+
+
+
+
+
+</body>
+</html>
+
